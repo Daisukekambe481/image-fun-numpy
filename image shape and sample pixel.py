81fdacea5db34afb3d5f341572a4ba79336bd283
Daisuke <219789162+Daisukekambe481@users.noreply.github.com>
2025-09-28 15:30:41 +0400

    Update image shape and sample pixel.py

--- image shape and sample pixel.py
+++ image shape and sample pixel.py
@@ -1,14 +1,1 @@
-import numpy as np
-from PIL import Image
 
-# Load/Open the image
-img = Image.open(r"C:\Users\Rameez\OneDrive\Projects\numpy project\image-fun-numpy\Sample image.jpg")
-
-# Convert to NumPy array
-img_array = np.array(img)
-
-# Print Image shape
-print("Image shape:", img_array.shape)
-
-# Print a small 5x5 block of pixels (rows 40-44, cols 40-44)
-print("5x5 block from (50,50):\n", img_array[40:45, 40:45])
